3adcaa55427a9049d2fb4bbfd2aa5ebe926d4d12
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-08 12:40:26 +0530

    Update upgrade-box.js

--- scripts/upgrade-box.js
+++ scripts/upgrade-box.js
@@ -1,11 +1,53 @@
-// manual way
+const {
+  developmentChains,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config");
+const { network, deployments, deployer } = require("hardhat");
+const { verify } = require("../helper-functions");
 
 async function main() {
-    const boxProxyAdmin = await ethers.getContract("BoxProxyAdmin")
-    const transparentProxy = await ethers.getContract("Box_Proxy")
-    // Hardhat deploy will just name the proxy -> implemenatation contract + _Proxy = Box_Proxy
-    const boxV2 = await ethers.getContract("BoxV2")
-    const upgradeTx = await
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+  log("----------------------------------------------------");
+
+  const boxV2 = await deploy("BoxV2", {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  });
+
+  // Verify the deployment
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log("Verifying...");
+    await verify(boxV2.address, []);
+  }
+
+  // Upgrade!
+  // Not "the hardhat-deploy way"
+  const boxProxyAdmin = await ethers.getContract("BoxProxyAdmin");
+  const transparentProxy = await ethers.getContract("Box_Proxy");
+  // Hardhat deploy will just name the proxy -> implemenatation contract + _Proxy = Box_Proxy
+  const upgradeTx = await boxProxyAdmin.upgrade(
+    transparentProxy.address,
+    boxV2.address
+  );
+  await upgradeTx.wait(1);
+  const proxyBox = await ethers.getContractAt(
+    "BoxV2",
+    transparentProxy.address
+  );
+  const version = await proxyBox.version();
+  console.log(version.toString());
+  log("----------------------------------------------------");
 }
 
 main()
